0fea05b347c2a3e80762008029ab024c9a46b645
ilya ilyushonok <ilyushonok2014@mail.ru>
2025-08-17 13:28:18 +0300

    ROC-23 fix:  login(sign-in) css

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -7,28 +7,26 @@ import {zodResolver} from "@hookform/resolvers/zod";
 import {z} from "zod";
 import gitHubSvg from "@/public/github-svg.svg"
 import googleSvg from "@/public/google-svg.svg"
-
+import styles from "@/src/feature/auth/styles/sign-in.module.css"
 
 const loginSchema = z.object({
     email: z.email("The email must match the format example@example.com"),
     password: z.string(),
 })
 
- export type LoginRequestParams = z.infer<typeof loginSchema>;
- export type LoginResponseAccessToken = {accessToken:string}
-
+export type LoginRequestParams = z.infer<typeof loginSchema>;
+export type LoginResponseAccessToken = { accessToken: string }
 
 
 export default function SignInPage() {
     const [login] = useLoginMutation()
-
     const dispatch = useAppDispatch()
-
 
     const {
         register,
         handleSubmit,
         formState: {errors},
+        reset
     } = useForm<LoginRequestParams>({
         resolver: zodResolver(loginSchema),
         defaultValues: {
@@ -38,34 +36,49 @@ export default function SignInPage() {
     })
 
     const onSubmit: SubmitHandler<LoginRequestParams> = async (data) => {
-        console.log("Отправка формы, сделается позже",data)
+        console.log("Отправка формы, сделается позже", data)
 
         try {
-            const res = await login(data).unwrap();
+            const res = await login(data).unwrap()
             console.log('res', res)
+            localStorage.setItem("accessToken", res.accessToken)
+            reset()
+
         } catch (error) {
-            console.log("ERRR", error)
+            console.log("ERROR", error)
         }
     };
 
     return (
-        <article>
+        <article className={styles.authWrapper}>
 
-            <form onSubmit={handleSubmit(onSubmit)}>
+            <form onSubmit={handleSubmit(onSubmit)} className={styles.authForm}>
                 <h2>Sign In</h2>
-                <div>
-                    <a href=""><Image src={googleSvg} alt={"Google"}  /></a>
+                <div className={styles.oAuth}>
+                    <a href=""><Image src={googleSvg} alt={"Google"}/></a>
                     <a href=""><Image src={gitHubSvg} alt={"GitHub"}/></a>
                 </div>
-                <input type="text" {...register("email")}  placeholder="Ulens@ulens.com"/>
-                {errors.email && <p>{errors.email.message}</p>}
 
-                <input type="text" {...register("password")} placeholder="**********"/>
-                {errors.password && <p>{errors.password.message}</p>}
+                <div className={styles.inputWrapper}>
 
-                <button>Sign In</button>
-                <span>Don’t have an account?</span>
-                <a href="">Sign Up</a>
+                <div className={styles.inputContainer}>
+                    <input type="text" {...register("email")} placeholder="Ulens@ulens.com" className={styles.emailInput} />
+                    {errors.email && <p>{errors.email.message}</p>}
+                </div>
+
+                <div className={styles.inputContainer}>
+                    <input type="text" {...register("password")} placeholder="**********" className={styles.passwordInput} />
+                    {errors.password && <p>{errors.password.message}</p>}
+                </div>
+
+                <button className={styles.btn}>Sign In</button>
+                </div>
+
+                    <span>Don’t have an account?</span>
+                    <a className={styles.signUpLink} href="">Sign Up</a>
+
+
+
             </form>
 
         </article>
