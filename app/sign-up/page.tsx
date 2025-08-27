65e218ccc346604a4a985f160798aed6b8fcbe54
Ilja <chylmanprosto@gmail.com>
2025-08-27 12:37:49 +0300

    feat: add FlexContainer

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,9 +1,12 @@
 'use client'
 
 import {SignUp} from "@/src/feature/auth/ui/SignUp";
+import {FlexContainer} from "@/src/common/components/FlexContainer";
 
 export default function SingUpPage() {
-  return <div>
-    <SignUp/>
-  </div>
+    return <div>
+        <FlexContainer justify={"center"} align={"center"} style={{minHeight: "100VH"}}>
+            <SignUp/>
+        </FlexContainer>
+    </div>
 }
