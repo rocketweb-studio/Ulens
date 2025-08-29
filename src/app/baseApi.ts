76621470eaaf64436a6af9c0e4a0894db476a4f5
Ilja <chylmanprosto@gmail.com>
2025-08-29 14:28:05 +0300

    feat: add useModal

--- src/app/baseApi.ts
+++ src/app/baseApi.ts
@@ -1,16 +1,12 @@
 import {fetchBaseQuery, createApi} from "@reduxjs/toolkit/query/react";
 
-
-
 console.log('process.env.NEXT_PUBLIC_BASE_URL', process.env.NEXT_PUBLIC_BASE_URL)
 
 export const baseApi = createApi({
-  reducerPath: "Ulens",
-  baseQuery: fetchBaseQuery({
-    baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
-    credentials: "include",
-  }),
-  endpoints: () => ({
-
-  }),
+    reducerPath: "Ulens",
+    baseQuery: fetchBaseQuery({
+        baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
+        credentials: "include",
+    }),
+    endpoints: () => ({}),
 })
