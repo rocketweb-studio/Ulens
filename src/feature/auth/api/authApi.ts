f0f1b02600261366f54a3816c9c8eed4f7f4cccf
Sergei-SLS <lyamin.sls@gmail.com>
2025-08-29 21:06:06 +0500

    refactor: logout endpoint

--- src/feature/auth/api/authApi.ts
+++ src/feature/auth/api/authApi.ts
@@ -3,41 +3,41 @@ import {UserType} from "@/src/feature/auth/api/authApi.types";
 import {LoginRequestParams, LoginResponseAccessToken} from "@/src/feature/auth/ui/SignIn";
 
 export const authApi = baseApi.injectEndpoints({
-  endpoints: (build) => ({
-    getUsers: build.query<UserType[], void>({
-      query: () => "auth/users",
+    endpoints: (build) => ({
+        getUsers: build.query<UserType[], void>({
+            query: () => "auth/users",
+        }),
+        registration: build.mutation<any, { userName: string, email: string, password: string }>({
+            query: (body) => ({method: 'post', url: 'auth/registration', body})
+        }),
+        confirmRegistration: build.mutation<any, { code: string }>({
+            query: (body) => ({method: 'post', url: 'auth/registration-confirmation', body})
+        }),
+        login: build.mutation<LoginResponseAccessToken, LoginRequestParams>({
+            query: (body) => ({method: "post", url: "auth/login", body}),
+        }),
+        passwordRecovery: build.mutation<any, { email: string }>({
+            query: (body) => ({method: "post", url: "auth/password-recovery", body}),
+        }),
+        checkRecoveryCode: build.mutation<any, { code: string }>({
+            query: (body) => ({method: "post", url: "auth/check-recovery-code", body}),
+        }),
+        setNewPassword: build.mutation<any, { password: string, code: string }>({
+            query: (body) => ({method: "post", url: "auth/new-password", body}),
+        }),
+        logout: build.mutation<LoginResponseAccessToken, LoginRequestParams>({
+            query: (body) => ({method: "post", url: "auth/logout", body})
+        })
     }),
-    registration: build.mutation<any, { userName: string, email: string, password: string }>({
-      query: (body) => ({method: 'post', url: 'auth/registration', body})
-    }),
-    confirmRegistration: build.mutation<any, { code: string }>({
-      query: (body) => ({method: 'post', url: 'auth/registration-confirmation', body})
-    }),
-    login: build.mutation<LoginResponseAccessToken, LoginRequestParams>({
-      query: (body) => ({method: "post", url: "auth/login", body}),
-    }),
-    passwordRecovery: build.mutation<any, { email: string }>({
-      query: (body) => ({method: "post", url: "auth/password-recovery", body}),
-    }),
-    checkRecoveryCode: build.mutation<any, { code: string }>({
-      query: (body) => ({method: "post", url: "auth/check-recovery-code", body}),
-    }),
-    setNewPassword: build.mutation<any, { password: string, code: string }>({
-      query: (body) => ({method: "post", url: "auth/new-password", body}),
-    }),
-
-    logout: build.mutation<LoginResponseAccessToken, LoginRequestParams>({
-        query: () => ({method: "delete", url: "auth/login"})
-    })
-  }),
 })
 
 export const {
-  useGetUsersQuery,
-  useLoginMutation,
-  usePasswordRecoveryMutation,
-  useRegistrationMutation,
-  useConfirmRegistrationMutation,
-  useCheckRecoveryCodeMutation,
-  useSetNewPasswordMutation
+    useGetUsersQuery,
+    useLoginMutation,
+    useLogoutMutation,
+    usePasswordRecoveryMutation,
+    useRegistrationMutation,
+    useConfirmRegistrationMutation,
+    useCheckRecoveryCodeMutation,
+    useSetNewPasswordMutation
 } = authApi
