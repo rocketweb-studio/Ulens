91b756dd9fc01197811823db9a9a132f520e8f1e
ilya ilyushonok <ilyushonok2014@mail.ru>
2025-08-30 11:07:56 +0300

    ROC-23 fix:fix store window

--- src/app/store.ts
+++ src/app/store.ts
@@ -16,6 +16,3 @@ setupListeners(store.dispatch)
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
 
-// для возможности обращения к store в консоли браузера
-// @ts-ignore
-window.store = store
