f1555fa83f9f6f3d480741c79eab7ec9647faaab
Sergei-SLS <lyamin.sls@gmail.com>
2025-09-01 16:18:16 +0500

    feat: create confirm logout

---
+++ src/feature/auth/ui/logout/ConfirmLogoutModal.tsx
@@ -0,0 +1,49 @@
+"use client"
+
+import { useRouter } from "next/navigation"
+import { Modal } from "@/src/common/components/Modal/Modal"
+import { Button } from "@/src/common/components/Button/Button"
+import {useLogoutMutation} from "@/src/feature/auth/api/authApi";
+import {toast} from "react-toastify";
+
+type Props = {
+    isOpen: boolean
+    onClose: () => void
+    email: string
+}
+
+export const ConfirmLogoutModal = ({ isOpen, onClose, email }: Props) => {
+    const router = useRouter()
+    const [logout] = useLogoutMutation()
+
+    const handleYes = async () => {
+        try {
+            await logout().unwrap()
+            onClose()
+            router.push("/login") // редирект на страницу логина
+        } catch (e) {
+            console.error("Logout error:", e)
+        }
+    }
+
+    const handleNo = () => {
+        toast.error(`User with this email doesn't exist`)
+        onClose()
+    }
+
+    return (
+        <Modal isOpen={isOpen} onClose={onClose} modalTitle="Confirm Logout">
+            <p>
+                Are you really want to log out of your account <b>{email}</b>?
+            </p>
+            <div style={{ display: "flex", gap: "1rem", marginTop: "1rem" }}>
+                <Button variant="primary" onClick={handleYes}>
+                    Yes
+                </Button>
+                <Button variant="secondary" onClick={handleNo}>
+                    No
+                </Button>
+            </div>
+        </Modal>
+    )
+}
--- src/feature/auth/ui/logout/ui/Logout.tsx
+++ src/feature/auth/ui/logout/ui/Logout.tsx
@@ -1,13 +1,19 @@
+import {useState} from "react";
+import {ConfirmLogoutModal} from "@/src/feature/auth/ui/logout/ConfirmLogoutModal";
 
 
-export const Logout = () => {
-
-    const handleLogout = () => {}
+export const Logout = ({ email }: { email: string }) => {
+    const [isModalOpen, setIsModalOpen] = useState(false)
 
     return (
-        <button onClick={handleLogout}>
-            Logout
-        </button>
+        <>
+            <button onClick={() => setIsModalOpen(true)}>Log out</button>
+            <ConfirmLogoutModal
+                isOpen={isModalOpen}
+                onClose={() => setIsModalOpen(false)}
+                email={email}
+            />
+        </>
     )
 }
 
