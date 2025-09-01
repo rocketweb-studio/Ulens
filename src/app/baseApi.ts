3deb468eac07ecc1c11456c352e14a6ef0c52944
OverTheTop404 <over_the_top_404@mail.ru>
2025-09-01 12:25:15 +0400

    ROC-26 fix: fix duplicate code setLoaderStatus

--- src/app/baseApi.ts
+++ src/app/baseApi.ts
@@ -1,29 +1,36 @@
 import {fetchBaseQuery, createApi} from "@reduxjs/toolkit/query/react";
 import {delay} from "@/src/common/utils";
 import {handleError} from "@/src/common/utils/handleError";
+import {setLoaderStatus} from "@/src/app/app-slice";
 
 export const baseApi = createApi({
   reducerPath: "Ulens",
 
   baseQuery: async (args, api, extraOptions) => {
-    if (args.url?.startsWith('auth/') || args === 'auth/me') {
+    api.dispatch(setLoaderStatus({ status: 'loading' }))
+    if (args.url?.includes('auth/')) {
       await delay(1000)
     }
-    const fetchResult = await fetchBaseQuery({
-      baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
-      credentials: "include",
-      prepareHeaders: (headers) => {
-        const token = localStorage.getItem('accessToken');
-        if (token) {
-          headers.set('Authorization', `Bearer ${token}`);
-        }
-        return headers;
-      },
-    })(args, api, extraOptions)
+    try {
+      const fetchResult = await fetchBaseQuery({
+        baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
+        credentials: "include",
+        prepareHeaders: (headers) => {
+          const token = localStorage.getItem('accessToken');
+          if (token) {
+            headers.set('Authorization', `Bearer ${token}`);
+          }
+          return headers;
+        },
+      })(args, api, extraOptions)
 
-    await handleError(api, fetchResult)
+      await handleError(api, fetchResult)
 
-    return fetchResult
+      return fetchResult
+    } finally {
+      api.dispatch(setLoaderStatus({ status: 'idle' }))
+    }
+
   },
   endpoints: () => ({
 
--- src/feature/auth/api/authApi.ts
+++ src/feature/auth/api/authApi.ts
@@ -1,113 +1,45 @@
 import {baseApi} from "@/src/app/baseApi";
 import {LoginRequestParams,LoginResponse,getMeResponse, UserType} from "@/src/feature/auth/api/authApi.types";
 import {RegistrationRequest, RegistrationResponce} from "@/src/feature/auth/types";
-import {setLoaderStatus} from "@/src/app/app-slice";
 
 // @ts-ignore
 export const authApi = baseApi.injectEndpoints({
     endpoints: (build) => ({
         getUsers: build.query<UserType[], void>({
             query: () => "auth/users",
-            onQueryStarted: async (_, { dispatch, queryFulfilled }) => {
-                dispatch(setLoaderStatus({ status: 'loading' }))
-                try {
-                    await queryFulfilled
-                } finally {
-                    dispatch(setLoaderStatus({ status: 'idle' }))
-                }
-            },
         }),
         registration: build.mutation<RegistrationResponce, RegistrationRequest>({
             query: (body) => ({method: "post", url: "auth/registration", body}),
-            onQueryStarted: async (_, { dispatch, queryFulfilled }) => {
-                dispatch(setLoaderStatus({ status: 'loading' }))
-                try {
-                    await queryFulfilled
-                } finally {
-                    dispatch(setLoaderStatus({ status: 'idle' }))
-                }
-            },
         }),
         confirmRegistration: build.mutation<any, { code: string }>({
             query: (body) => ({method: 'post', url: 'auth/registration-confirmation', body}),
-            onQueryStarted: async (_, { dispatch, queryFulfilled }) => {
-                dispatch(setLoaderStatus({ status: 'loading' }))
-                try {
-                    await queryFulfilled
-                } finally {
-                    dispatch(setLoaderStatus({ status: 'idle' }))
-                }
-            },
         }),
         resendRegistrationEmail: build.mutation<any, { email: string, recaptchaToken: string }>({
             query: (body) => ({method: 'post', url: 'auth/registration-email-resending', body}),
-            onQueryStarted: async (_, { dispatch, queryFulfilled }) => {
-                dispatch(setLoaderStatus({ status: 'loading' }))
-                try {
-                    await queryFulfilled
-                } finally {
-                    dispatch(setLoaderStatus({ status: 'idle' }))
-                }
-            },
         }),
         login: build.mutation<LoginResponse, LoginRequestParams>({
             query: (body) => ({method: "post", url: "auth/login", body}),
             async onQueryStarted(_arg, {dispatch, queryFulfilled}) {
                 try {
-                    dispatch(setLoaderStatus({ status: 'loading' }))
                     const res = await queryFulfilled;
                     localStorage.setItem("accessToken", res.data.accessToken)
                     await dispatch(authApi.endpoints.getMe.initiate());
                 } catch (error) {
                     console.log("login endpoint err: ", error)
-                } finally {
-                    dispatch(setLoaderStatus({ status: 'idle' }))
                 }
             },
         }),
         getMe: build.query<getMeResponse, void>({
             query: () => "auth/me",
-            onQueryStarted: async (_, { dispatch, queryFulfilled }) => {
-                dispatch(setLoaderStatus({ status: 'loading' }))
-                try {
-                    await queryFulfilled
-                } finally {
-                    dispatch(setLoaderStatus({ status: 'idle' }))
-                }
-            },
         }),
         passwordRecovery: build.mutation<any, { email: string, recaptchaToken: string }>({
             query: (body) => ({method: "post", url: "auth/password-recovery", body}),
-            onQueryStarted: async (_, { dispatch, queryFulfilled }) => {
-                dispatch(setLoaderStatus({ status: 'loading' }))
-                try {
-                    await queryFulfilled
-                } finally {
-                    dispatch(setLoaderStatus({ status: 'idle' }))
-                }
-            },
         }),
         checkRecoveryCode: build.mutation<any, { code: string }>({
             query: (body) => ({method: "post", url: "auth/check-recovery-code", body}),
-            onQueryStarted: async (_, { dispatch, queryFulfilled }) => {
-                dispatch(setLoaderStatus({ status: 'loading' }))
-                try {
-                    await queryFulfilled
-                } finally {
-                    dispatch(setLoaderStatus({ status: 'idle' }))
-                }
-            },
         }),
         setNewPassword: build.mutation<any, { newPassword: string, recoveryCode: string }>({
             query: (body) => ({method: "post", url: "auth/new-password", body}),
-            onQueryStarted: async (_, { dispatch, queryFulfilled }) => {
-                dispatch(setLoaderStatus({ status: 'loading' }))
-                try {
-                    await queryFulfilled
-                } finally {
-                    dispatch(setLoaderStatus({ status: 'idle' }))
-                }
-            },
         }),
 
     }),
