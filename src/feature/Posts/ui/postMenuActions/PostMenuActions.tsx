3272ccdbb210da22f001aed061b16c21ae35e7a8
Sergei-SLS <lyamin.sls@gmail.com>
2025-09-13 13:30:34 +0500

    refactor: change structure PostMenuActions

--- src/feature/Posts/ui/postMenuActions/PostMenuActions.tsx
+++ src/feature/Posts/ui/postMenuActions/PostMenuActions.tsx
@@ -1,15 +1,9 @@
 'use client'
 
 import { useState } from 'react'
-import { Modal } from '@/src/shared/components/Modal/Modal'
-import { Button } from '@/src/shared/components/Button/Button'
-import { useDeletePostMutation } from '@/src/feature/Posts/api/postsApi'
-import { useRouter } from 'next/navigation'
 import s from './postMenuActions.module.scss'
-import Image from 'next/image'
-import editIcon from '@/public/edit.svg'
-import deleteIcon from '@/public/trash.svg'
-import { Path } from '@/src/shared/constants/Path'
+import { PostDeleteModal } from '@/src/feature/Posts/ui/postDeleteModal'
+import { IconEdit2, IconTrash } from '@rocketweb-studio/ulens-ui-kit'
 
 type Props = {
   postId: string
@@ -17,19 +11,7 @@ type Props = {
 
 export const PostMenuActions = ({ postId }: Props) => {
   const [menuOpen, setMenuOpen] = useState(false)
-  const [isOpen, setIsOpen] = useState(false)
-  const [deletePost, { isLoading }] = useDeletePostMutation()
-  const router = useRouter()
-
-  const handleDelete = async () => {
-    try {
-      await deletePost(postId).unwrap()
-      setIsOpen(false)
-      router.push(Path.Profile)
-    } catch (e) {
-      console.error('Ошибка удаления поста', e)
-    }
-  }
+  const [deleteModalOpen, setDeleteModalOpen] = useState(false)
 
   return (
     <div className={s.container}>
@@ -42,27 +24,23 @@ export const PostMenuActions = ({ postId }: Props) => {
       {menuOpen && (
         <div className={s.menu}>
           <button className={s.menuItem} onClick={() => alert('Edit post clicked')}>
-            <Image src={editIcon} alt='edit' width={16} height={16} />
+            <IconEdit2 width={16} height={16} />
             Edit Post
           </button>
-          <button className={s.menuItem} onClick={() => setIsOpen(true)}>
-            <Image src={deleteIcon} alt='delete' width={16} height={16} />
+          <button
+            className={s.menuItem}
+            onClick={() => {
+              setMenuOpen(false)
+              setDeleteModalOpen(true)
+            }}
+          >
+            <IconTrash width={16} height={16} />
             Delete Post
           </button>
         </div>
       )}
 
-      <Modal isOpen={isOpen} onClose={() => setIsOpen(false)} modalTitle='Delete Post' hideDefaultButton>
-        <p>Are you sure you want to delete this post?</p>
-        <div className={s.modalButtons}>
-          <Button onClick={() => setIsOpen(false)} disabled={isLoading}>
-            No
-          </Button>
-          <Button onClick={handleDelete} disabled={isLoading}>
-            Yes
-          </Button>
-        </div>
-      </Modal>
+      <PostDeleteModal postId={postId} isOpen={deleteModalOpen} onClose={() => setDeleteModalOpen(false)} />
     </div>
   )
 }
