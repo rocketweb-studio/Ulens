c9c27544ed3bf332c1dc4f0ee97ad3ce34aeb02c
Sergei-SLS <lyamin.sls@gmail.com>
2025-09-23 23:28:27 +0500

    style: add PostMenuActions in ViewPostModal and add transition on hover

--- src/shared/components/ViewPostModal/ViewPostModal.tsx
+++ src/shared/components/ViewPostModal/ViewPostModal.tsx
@@ -1,19 +1,20 @@
 'use client'
 
-import {useModal} from '@/src/shared/hooks/useModal'
-import {FlexContainer} from '@/src/shared/components/FlexContainer'
-import {useRouter} from 'next/navigation'
-import {Path} from '@/src/shared/constants/Path'
-import s from "./VewPostModal.module.scss";
-import Image from "next/image";
-import {Modal} from "@/src/shared/components/Modal/Modal";
-import {MouseEvent} from "react";
-import {useGetPostByIdQuery} from "@/src/feature/Posts/api/postsApi";
-import {CustomSwiper} from "@/src/shared/components/CustomSwiper";
-import {useGetProfileByUsedIdQuery} from "@/src/feature/userProfile/api/userProfileApi";
-import Link from "next/link";
+import { useModal } from '@/src/shared/hooks/useModal'
+import { FlexContainer } from '@/src/shared/components/FlexContainer'
+import { useRouter } from 'next/navigation'
+import { Path } from '@/src/shared/constants/Path'
+import s from './VewPostModal.module.scss'
+import Image from 'next/image'
+import { Modal } from '@/src/shared/components/Modal/Modal'
+import { MouseEvent } from 'react'
+import { useGetPostByIdQuery } from '@/src/feature/Posts/api/postsApi'
+import { CustomSwiper } from '@/src/shared/components/CustomSwiper'
+import { useGetProfileByUsedIdQuery } from '@/src/feature/userProfile/api/userProfileApi'
+import Link from 'next/link'
+import { PostMenuActions } from '@/src/feature/Posts/ui/postMenuActions'
 
-export default function ViewPostModal({ userId, postId }: { userId: string, postId: string }) {
+export default function ViewPostModal({ userId, postId }: { userId: string; postId: string }) {
   const { isOpen, closeModal } = useModal(true)
   const { replace } = useRouter()
 
@@ -28,13 +29,13 @@ export default function ViewPostModal({ userId, postId }: { userId: string, post
       replace(Path.Profile)
     }
   }
-  const {data: postInfo} = useGetPostByIdQuery({postId})
-  const {data: user} = useGetProfileByUsedIdQuery({userId})
+  const { data: postInfo } = useGetPostByIdQuery({ postId })
+  const { data: user } = useGetProfileByUsedIdQuery({ userId })
 
   return (
     <FlexContainer align={'center'} justify={'center'}>
       <div className={s.wrapper}>
-      <Modal
+        <Modal
           className={`${s.modal} ${s.viewPostModal}`}
           isOpen={isOpen}
           onClose={onModalCloseHandler}
@@ -43,44 +44,51 @@ export default function ViewPostModal({ userId, postId }: { userId: string, post
           withoutPadding
           hideCloseButton
           hideDefaultButton
-      >
-        <div className={s.publication}>
-          <div className={s.publicationImg}>
-            {postInfo?.images &&
+        >
+          <div className={s.publication}>
+            <div className={s.publicationImg}>
+              {postInfo?.images && (
                 <CustomSwiper
-                    slides={
-                      postInfo?.images.map((image, index) => ({
-                        id: index,
-                        content: (
-                            <Image className={s.zaebalaimg} src={`${process.env.NEXT_PUBLIC_MEDIA_URL}${image.url}`} alt={''} fill  style={{ objectFit: 'cover' }}/>
-                        ),
-                      }))
-                    }
-                    navigation={true}
-                    pagination={true}
-                    className={s.customSwiper}
-                    allowTouchMove={false}
-                    swiperProps={{
-                      spaceBetween: 0,
-                      slidesPerView: 1,
-                      initialSlide: 0,
-                      noSwiping: true,
-                      noSwipingClass: 'swiper-slide',
-                      preventInteractionOnTransition: true,
-                    }}
+                  slides={postInfo?.images.map((image, index) => ({
+                    id: index,
+                    content: (
+                      <Image
+                        className={s.zaebalaimg}
+                        src={`${process.env.NEXT_PUBLIC_MEDIA_URL}${image.url}`}
+                        alt={''}
+                        fill
+                        style={{ objectFit: 'cover' }}
+                      />
+                    ),
+                  }))}
+                  navigation={true}
+                  pagination={true}
+                  className={s.customSwiper}
+                  allowTouchMove={false}
+                  swiperProps={{
+                    spaceBetween: 0,
+                    slidesPerView: 1,
+                    initialSlide: 0,
+                    noSwiping: true,
+                    noSwipingClass: 'swiper-slide',
+                    preventInteractionOnTransition: true,
+                  }}
                 />
-            }
-          </div>
-          <div className={s.publicationContent}>
-            <div className={s.publicationProfile}>
-              <div className={s.publicationProfileImage}>
-                <Image src={'/avatar/avatar_mini.png'} alt={'Avatar'} width={36} height={36} />
-                <Link href={Path.UserProfile(userId)} className={s.publicationProfileURL}>{user?.userName}</Link>
+              )}
+            </div>
+            <div className={s.publicationContent}>
+              <div className={s.publicationProfile}>
+                <div className={s.publicationProfileImage}>
+                  <Image src={'/avatar/avatar_mini.png'} alt={'Avatar'} width={36} height={36} />
+                  <Link href={Path.UserProfile(userId)} className={s.publicationProfileURL}>
+                    {user?.userName}
+                  </Link>
+                  <PostMenuActions postId={userId} description={''} />
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </Modal>
+        </Modal>
       </div>
     </FlexContainer>
   )
