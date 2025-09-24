68064120124da94aa004de7307d57fea7cb0542c
Sergei-SLS <lyamin.sls@gmail.com>
2025-09-24 21:05:38 +0500

    refactor: add new modal in PostEditModal

--- src/feature/Posts/ui/postEditModal/PostEditModal.tsx
+++ src/feature/Posts/ui/postEditModal/PostEditModal.tsx
@@ -3,6 +3,12 @@ import { useUpdatePostMutation } from '@/src/feature/Posts/api/postsApi'
 import { toast } from 'react-toastify'
 import { Modal } from '@/src/shared/components/Modal/Modal'
 import s from './postEditModal.module.scss'
+import { Button } from '@/src/shared/components/Button/Button'
+import { IconArrowIosBackOutline } from '@rocketweb-studio/ulens-ui-kit'
+import { CustomSwiper } from '@/src/shared/components/CustomSwiper'
+import Image from 'next/image'
+import { Controller } from 'react-hook-form'
+import { TextArea } from '@/src/shared/components/TextArea/TextArea'
 
 type Props = {
   postId: string
@@ -43,19 +49,52 @@ export const PostEditModal = ({ postId, initialDescription, isOpen, onClose }: P
 
   return (
     <>
-      <Modal isOpen={isOpen} onClose={handleConfirmClose} modalTitle={'Edit Post'} hideDefaultButton>
-        <p>Add publication descriptions</p>
-        <textarea
-          className={s.textarea}
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          maxLength={500}
-        />
-        <div className={s.counter}>{description?.length ?? 0}/500</div>
-        <div className={s.actions}>
-          <button disabled={isLoading} onClick={handleSave}>
-            Save Changes
-          </button>
+      {/*<Modal isOpen={isOpen} onClose={handleConfirmClose} modalTitle={'Edit Post'} hideDefaultButton>*/}
+      {/*  <p>Add publication descriptions</p>*/}
+      {/*  <textarea*/}
+      {/*    className={s.textarea}*/}
+      {/*    value={description}*/}
+      {/*    onChange={(e) => setDescription(e.target.value)}*/}
+      {/*    maxLength={500}*/}
+      {/*  />*/}
+      {/*  <div className={s.counter}>{description?.length ?? 0}/500</div>*/}
+      {/*  <div className={s.actions}>*/}
+      {/*    <button disabled={isLoading} onClick={handleSave}>*/}
+      {/*      Save Changes*/}
+      {/*    </button>*/}
+      {/*  </div>*/}
+      {/*</Modal>*/}
+
+      <Modal isOpen={isOpen} onClose={handleConfirmClose} modalTitle='Edit Post' hideDefaultButton>
+        <div className={s.wrapper}>
+          {/* Левая колонка с картинкой */}
+          <div className={s.imageColumn}>
+            <Image src='/post-example.png' alt='Post image' width={600} height={600} className={s.postImage} />
+          </div>
+
+          {/* Правая колонка */}
+          <div className={s.contentColumn}>
+            <div className={s.profile}>
+              <Image src='/avatar/avatar_mini.png' alt='Avatar' width={36} height={36} className={s.avatar} />
+              <p className={s.userName}>UserName</p>
+            </div>
+
+            <label className={s.label}>Add publication descriptions</label>
+
+            <textarea
+              className={s.textarea}
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              maxLength={500}
+            />
+
+            <div className={s.footer}>
+              <span className={s.counter}>{description.length}/500</span>
+              <button disabled={isLoading} className={s.saveButton} onClick={handleSave}>
+                Save Changes
+              </button>
+            </div>
+          </div>
         </div>
       </Modal>
 
