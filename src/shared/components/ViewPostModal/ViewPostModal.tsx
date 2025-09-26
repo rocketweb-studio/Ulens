e1a0a08c6bb96aa3dca195476b475e2fc54ecf88
ilya ilyushonok <ilyushonok2014@mail.ru>
2025-09-26 14:08:21 +0300

    feat:ViewPostModal added post actions

--- src/shared/components/ViewPostModal/ViewPostModal.tsx
+++ src/shared/components/ViewPostModal/ViewPostModal.tsx
@@ -1,22 +1,20 @@
 'use client'
 
-import { useModal } from '@/src/shared/hooks/useModal'
-import { FlexContainer } from '@/src/shared/components/FlexContainer'
-import { useRouter } from 'next/navigation'
-import { Path } from '@/src/shared/constants/Path'
+import {useModal} from '@/src/shared/hooks/useModal'
+import {FlexContainer} from '@/src/shared/components/FlexContainer'
+import {useRouter} from 'next/navigation'
+import {Path} from '@/src/shared/constants/Path'
 import s from './ViewPostModal.module.scss'
 import Image from 'next/image'
-import { Modal } from '@/src/shared/components/Modal/Modal'
-import { MouseEvent } from 'react'
-import { useGetPostByIdQuery } from '@/src/feature/Posts/api/postsApi'
-import { CustomSwiper } from '@/src/shared/components/CustomSwiper'
-import { useGetProfileByUsedIdQuery } from '@/src/feature/userProfile/api/userProfileApi'
+import {Modal} from '@/src/shared/components/Modal/Modal'
+import {MouseEvent} from 'react'
+import {useGetPostByIdQuery} from '@/src/feature/Posts/api/postsApi'
+import {CustomSwiper} from '@/src/shared/components/CustomSwiper'
+import {useGetProfileByUsedIdQuery} from '@/src/feature/userProfile/api/userProfileApi'
 import Link from 'next/link'
-import { PostMenuActions } from '@/src/feature/Posts/ui/postMenuActions'
-import {
-  IconHeart,
-  IconHeartOutline
-} from '@rocketweb-studio/ulens-ui-kit'
+import {PostMenuActions} from '@/src/feature/Posts/ui/postMenuActions'
+import {IconHeart, IconHeartOutline} from '@rocketweb-studio/ulens-ui-kit'
+import {useGetMeQuery} from "@/src/feature/auth/api/authApi";
 
 const comments = [
   {id: 1, authorImage: '/avatar/avatar_mini.png', userName: 'UserName', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', isChecked: false, date: '2 hours ago', likesCount: 0},
@@ -29,6 +27,7 @@ const comments = [
 export default function ViewPostModal({ userId, postId }: { userId: string; postId: string }) {
   const { isOpen, closeModal } = useModal(true)
   const { replace,  } = useRouter()
+  const {data} = useGetMeQuery()
 
   const onModalCloseHandler = () => {
     closeModal()
@@ -114,16 +113,23 @@ export default function ViewPostModal({ userId, postId }: { userId: string; post
                         <div className={s.commentPanel}>
                           <span className={s.date}>{comment.date}</span>
                           {comment.likesCount > 0 && <span className={s.like}>Like: {comment.likesCount}</span>}
-                          <span className={s.like}>Answer</span>
+                          {data && <span className={s.like}>Answer</span>}
                         </div>
                       </div>
-                      {comment.isChecked
+                      {data && (comment.isChecked
                           ? <div className={s.iconHeart}><IconHeart/></div>
                           : <div className={s.iconHeartOutline}><IconHeartOutline/></div>
-                      }
+                      )}
                     </div>
                 ))}
               </div>
+              {data && <div className={s.postActions}>
+                <div className={s.postActionsLeft}>
+                  <IconHeartOutline/>
+                  <Image width={24} height={24} src={"/SavedPost.svg"} alt={'Saved'}/>
+                </div>
+                <Image width={24} height={24} src={"/SendPost.svg"} alt={'Saved'}/>
+              </div>}
             </div>
           </div>
         </Modal>
