f981d264af3ba290d0d226e42ec882346c6aa13c
Ilja Afanasev <chylmanprosto@gmail.com>
2025-09-28 18:16:23 +0300

    fix: fix remove scroll in modal

--- src/shared/components/CreatePostModal/CreatePostModal.tsx
+++ src/shared/components/CreatePostModal/CreatePostModal.tsx
@@ -4,15 +4,28 @@ import { useModal } from '@/src/shared/hooks/useModal'
 import { FlexContainer } from '@/src/shared/components/FlexContainer'
 import { PostCreateModal } from '@/src/feature/postCreate/ui/PostCreateModal/PostCreateModal'
 import { useRouter } from 'next/navigation'
-import { Path } from '@/src/shared/constants/Path'
 
 export default function CreatePostModal() {
   const { isOpen, closeModal, openModal } = useModal(true)
-  const { replace } = useRouter()
+  const router = useRouter()
+
+  const updateSearchParams = (newParams: Record<string, string>) => {
+    const searchParams = new URLSearchParams(window.location.search)
+
+    Object.entries(newParams).forEach(([key, value]) => {
+      if (value) {
+        searchParams.set(key, value)
+      } else {
+        searchParams.delete(key)
+      }
+    })
+
+    router.replace(`?${searchParams.toString()}`, { scroll: false })
+  }
 
   const onModalCloseHandler = () => {
     closeModal()
-    replace(Path.Profile)
+    updateSearchParams({ action: '' })
   }
 
   return (
