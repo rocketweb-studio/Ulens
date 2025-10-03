0b4c2da428302b631e2f347444db76a8c56afce3
YuryR <myaccount135@gmail.com>
2025-10-03 21:53:31 +0300

    fix: protect route settings/page.tsx

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,26 +1,32 @@
-import { redirect } from 'next/navigation'
+'use client'
+
+import {redirect, useSearchParams} from 'next/navigation'
 import {Path} from "@/src/shared/constants/Path";
 import {Tabs} from "@/src/shared/components/Tabs";
+import {useGetMeQuery} from "@/src/feature/auth/api/authApi";
 
 const allowedParts = ['info', 'devices', 'subscriptions', 'payments']
 
-export default async function SettingsPage({
-  searchParams,
-}: {
-  searchParams: Promise<{ [key: string]: string | undefined }>
-}) {
-  const params = await searchParams
-  if (!params.part || !allowedParts.includes(params.part)) {
+export default function SettingsPage() {
+  const params = useSearchParams()
+  const {data: meData} = useGetMeQuery()
+  const part = params.get('part')
+
+  if (!meData) {
+    redirect(Path.SignIn)
+  }
+
+  if (!part || !allowedParts.includes(part)) {
     redirect(Path.Settings('info'))
   }
 
   return (
     <div>
       <Tabs/>
-      {params.part === 'info' && <p>Текущий раздел: {params.part}</p>}
-      {params.part === 'devices' && <p>Текущий раздел: {params.part}</p>}
-      {params.part === 'subscriptions' && <p>Текущий раздел: {params.part}</p>}
-      {params.part === 'payments' && <p>Текущий раздел: {params.part}</p>}
+      {part === 'info' && <p>Текущий раздел: {part}</p>}
+      {part === 'devices' && <p>Текущий раздел: {part}</p>}
+      {part === 'subscriptions' && <p>Текущий раздел: {part}</p>}
+      {part === 'payments' && <p>Текущий раздел: {part}</p>}
     </div>
   )
 }
