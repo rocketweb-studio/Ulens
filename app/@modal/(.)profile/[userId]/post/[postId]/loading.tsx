2f6779cf638debce531c08841fbdadebbe4e793a
OverTheTop404 <over_the_top_404@mail.ru>
2025-10-10 00:53:07 +0400

    ROC-51 feat: set timout (100ms) before loader show for fast request

--- app/@modal/(.)profile/[userId]/post/[postId]/loading.tsx
+++ app/@modal/(.)profile/[userId]/post/[postId]/loading.tsx
@@ -3,8 +3,18 @@
 import s from '@/src/widgets/ViewPostModal/ui/ViewPostModal.module.scss'
 import { AppLoader } from '@/src/shared/ui/AppLoader/AppLoader'
 import { Modal } from '@/src/shared/ui/Modal/Modal'
+import { useEffect, useState } from 'react'
 
 export default function Loading() {
+  const [show, setShow] = useState(false)
+
+  useEffect(() => {
+    const timer = setTimeout(() => setShow(true), 100)
+    return () => clearTimeout(timer)
+  }, [])
+
+  if (!show) return null
+
   return (
     <Modal
       className={`${s.modal} ${s.viewPostModal}`}
