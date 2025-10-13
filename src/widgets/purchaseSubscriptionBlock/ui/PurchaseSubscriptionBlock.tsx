54f4f006491d5714f1eb6006591722e261bc6c52
Ilja Afanasev <chylmanprosto@gmail.com>
2025-10-13 21:45:11 +0300

    feat: add hide purchaseSubscription when user have subscribe

--- src/widgets/purchaseSubscriptionBlock/ui/PurchaseSubscriptionBlock.tsx
+++ src/widgets/purchaseSubscriptionBlock/ui/PurchaseSubscriptionBlock.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useGetPlansQuery } from '@/src/entities/payments'
+import { useGetMySubscriptionQuery, useGetPlansQuery } from '@/src/entities/payments'
 import s from './purchaseSubscriptionBlock.module.scss'
 import { MakePayment } from '@/src/features/payments/makePayment'
 import { Card, FlexContainer } from '@/src/shared/ui'
@@ -13,40 +13,44 @@ export const PurchaseSubscriptionBlock = () => {
   const [accountType, setAccountType] = useState<AccountType>('personal')
   const [subscriptionVariant, setSubscriptionVariant] = useState<SubscriptionVariant>('1')
   const { data: plans } = useGetPlansQuery()
+  const { data: subscriptionData, isError } = useGetMySubscriptionQuery()
   const plansRadioButtons = plans ? plans.map((plan) => ({ value: plan.id.toString(), label: plan.title })) : []
 
   return (
-    <FlexContainer direction={'column'} gap={'30px'}>
-      <section>
-        <h2 className={s.title}>Account type:</h2>
-        <Card contentClass={s.accountTypeCardContent}>
-          <RadioButtonsGroup
-            name={'account-type'}
-            value={accountType}
-            onChange={(e) => setAccountType(e as AccountType)}
-            options={[
-              { value: 'personal', label: 'Personal' },
-              { value: 'business', label: 'Business' },
-            ]}
-          ></RadioButtonsGroup>
-        </Card>
-      </section>
-      {accountType === 'business' && (
+    !subscriptionData &&
+    isError && (
+      <FlexContainer direction={'column'} gap={'30px'}>
         <section>
-          <h2 className={s.title}>Your subscription costs:</h2>
+          <h2 className={s.title}>Account type:</h2>
           <Card contentClass={s.accountTypeCardContent}>
             <RadioButtonsGroup
-              name={'subscription-variant'}
-              value={subscriptionVariant}
-              onChange={(e) => setSubscriptionVariant(e as SubscriptionVariant)}
-              options={plansRadioButtons}
+              name={'account-type'}
+              value={accountType}
+              onChange={(e) => setAccountType(e as AccountType)}
+              options={[
+                { value: 'personal', label: 'Personal' },
+                { value: 'business', label: 'Business' },
+              ]}
             ></RadioButtonsGroup>
           </Card>
-          <FlexContainer className={s.paymentsButtonsGroup} justify={'end'}>
-            <MakePayment planId={Number(subscriptionVariant)}></MakePayment>
-          </FlexContainer>
         </section>
-      )}
-    </FlexContainer>
+        {accountType === 'business' && (
+          <section>
+            <h2 className={s.title}>Your subscription costs:</h2>
+            <Card contentClass={s.accountTypeCardContent}>
+              <RadioButtonsGroup
+                name={'subscription-variant'}
+                value={subscriptionVariant}
+                onChange={(e) => setSubscriptionVariant(e as SubscriptionVariant)}
+                options={plansRadioButtons}
+              ></RadioButtonsGroup>
+            </Card>
+            <FlexContainer className={s.paymentsButtonsGroup} justify={'end'}>
+              <MakePayment planId={Number(subscriptionVariant)}></MakePayment>
+            </FlexContainer>
+          </section>
+        )}
+      </FlexContainer>
+    )
   )
 }
