3d8aa6238e3cd9ff0908ef5bff4384f6fddc6ccc
YuryR <myaccount135@gmail.com>
2025-10-18 12:37:45 +0300

    feat: add a success message on useUpdateProfileMutation

--- src/entities/userProfile/ui/UpProfileInfo/UpProfileInfo.tsx
+++ src/entities/userProfile/ui/UpProfileInfo/UpProfileInfo.tsx
@@ -11,166 +11,170 @@ import {useGetProfileByUsedIdQuery, useUpdateProfileMutation} from '@/src/entiti
 import {DatePicker} from "@/src/shared/ui/DataPicker/DatePicker";
 import {zodResolver} from "@hookform/resolvers/zod";
 import {useGetMeQuery} from '@/src/entities/auth/api/authApi'
+import {toast} from "react-toastify";
 
 
 const countriesCities: Record<string, string[]> = {
-    Belarus: ['Minsk', 'Brest', 'Grodno', 'Gomel', 'Mogilev', 'Vitebsk'],
-    Russia: ['Moscow', 'Saint Petersburg', 'Novosibirsk', 'Yekaterinburg', 'Kazan', 'Nizhny Novgorod'],
-    USA: ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix', 'Philadelphia'],
-    Germany: ['Berlin', 'Munich', 'Hamburg', 'Cologne', 'Frankfurt', 'Stuttgart'],
-    Poland: ['Warsaw', 'Krakow', 'Lodz', 'Wroclaw', 'Poznan', 'Gdansk'],
-    Ukraine: ['Kyiv', 'Kharkiv', 'Odesa', 'Dnipro', 'Lviv', 'Donetsk'],
-    France: ['Paris', 'Marseille', 'Lyon', 'Toulouse', 'Nice', 'Nantes'],
-    Italy: ['Rome', 'Milan', 'Naples', 'Turin', 'Florence', 'Venice'],
+  Belarus: ['Minsk', 'Brest', 'Grodno', 'Gomel', 'Mogilev', 'Vitebsk'],
+  Russia: ['Moscow', 'Saint Petersburg', 'Novosibirsk', 'Yekaterinburg', 'Kazan', 'Nizhny Novgorod'],
+  USA: ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix', 'Philadelphia'],
+  Germany: ['Berlin', 'Munich', 'Hamburg', 'Cologne', 'Frankfurt', 'Stuttgart'],
+  Poland: ['Warsaw', 'Krakow', 'Lodz', 'Wroclaw', 'Poznan', 'Gdansk'],
+  Ukraine: ['Kyiv', 'Kharkiv', 'Odesa', 'Dnipro', 'Lviv', 'Donetsk'],
+  France: ['Paris', 'Marseille', 'Lyon', 'Toulouse', 'Nice', 'Nantes'],
+  Italy: ['Rome', 'Milan', 'Naples', 'Turin', 'Florence', 'Venice'],
 }
 
 
 export const UpProfileInfo = () => {
-  const [updateProfile, { isLoading }] = useUpdateProfileMutation()
-  const { data: me } = useGetMeQuery()
-  const { data: dataProfile, isSuccess } = useGetProfileByUsedIdQuery({ userId: me?.id! }, { skip: !me?.id })
+  const [updateProfile, {isLoading}] = useUpdateProfileMutation()
+  const {data: me} = useGetMeQuery()
+  const {data: dataProfile, isSuccess} = useGetProfileByUsedIdQuery({userId: me?.id!}, {skip: !me?.id})
 
-    const {
-        register,
-        handleSubmit,
-        reset,
-        control,
-        setValue,
-        formState: { errors },
-    } = useForm<UserProfile>({
-        resolver: zodResolver(profileSchema),
-        defaultValues: {
-            userName: '',
-            firstName: '',
-            lastName: '',
-            city: '',
-            country: '',
-            dateOfBirth: '',
-            aboutMe: '',
-        },
-    })
+  const {
+    register,
+    handleSubmit,
+    reset,
+    control,
+    setValue,
+    formState: {errors},
+  } = useForm<UserProfile>({
+    resolver: zodResolver(profileSchema),
+    defaultValues: {
+      userName: '',
+      firstName: '',
+      lastName: '',
+      city: '',
+      country: '',
+      dateOfBirth: '',
+      aboutMe: '',
+    },
+  })
 // Отслеживаем изменение страны
-    const selectedCountry = useWatch({
-        control,
-        name: 'country',
-    })
+  const selectedCountry = useWatch({
+    control,
+    name: 'country',
+  })
 
-    // Получаем города для выбранной страны
-    const getCitiesForCountry = (country: string): string[] => {
-        return countriesCities[country] || []
+  // Получаем города для выбранной страны
+  const getCitiesForCountry = (country: string): string[] => {
+    return countriesCities[country] || []
+  }
+
+  // Очищаем город при смене страны
+  useEffect(() => {
+    if (selectedCountry) {
+      setValue('city', '')
     }
+  }, [selectedCountry, setValue])
 
-    // Очищаем город при смене страны
-    useEffect(() => {
-        if (selectedCountry) {
-            setValue('city', '')
-        }
-    }, [selectedCountry, setValue])
-
-    useEffect(() => {
-        if (dataProfile) {
-            reset(dataProfile)
-        }
-    }, [isSuccess])
-
-    const onSubmit: SubmitHandler<UserProfile> = async (data) => {
-        console.log('submit')
-        try {
-            await updateProfile(data).unwrap()
-        } catch (e) {
-            console.error('Error updating profile:', e)
-        }
+  useEffect(() => {
+    if (dataProfile) {
+      reset(dataProfile)
     }
-    return (
-        <form onSubmit={handleSubmit(onSubmit)} className={s.inputsContainer}>
-            <Input
-                type='text'
-                label={'Username'}
-                name={'userName'}
-                required
-                register={register}
-                error={errors.userName?.message}
+  }, [isSuccess])
+
+  const onSubmit: SubmitHandler<UserProfile> = async (data) => {
+    console.log('submit')
+    try {
+      await updateProfile(data).unwrap()
+      toast.success('Profile updated successfully.')
+    } catch (e) {
+      console.error('Error updating profile:', e)
+    }
+  }
+
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className={s.inputsContainer}>
+      <Input
+        type='text'
+        label={'Username'}
+        name={'userName'}
+        required
+        register={register}
+        error={errors.userName?.message}
+      />
+      <Input
+        type='text'
+        label={'First Name'}
+        name={'firstName'}
+        required
+        register={register}
+        error={errors.firstName?.message}
+      />
+      <Input
+        type='text'
+        label={'Last Name'}
+        name={'lastName'}
+        required
+        register={register}
+        error={errors.lastName?.message}
+      />
+
+      <Controller
+        name='dateOfBirth'
+        control={control}
+        rules={{required: true}}
+        render={({field}) => {
+          return (
+            <DatePicker
+              label={'Date of birth'}
+              labelMobile={'Date of birthday'}
+              error={errors.dateOfBirth?.message}
+              errorLink={<Link href={Path.PrivacyPolicy}>PrivacyPolicy</Link>}
+              {...field}
             />
-            <Input
-                type='text'
-                label={'First Name'}
-                name={'firstName'}
-                required
-                register={register}
-                error={errors.firstName?.message}
-            />
-            <Input
-                type='text'
-                label={'Last Name'}
-                name={'lastName'}
-                required
-                register={register}
-                error={errors.lastName?.message}
-            />
+          )
+        }}
+      />
 
-            <Controller
-                name='dateOfBirth'
-                control={control}
-                rules={{required: true}}
-                render={({field}) => {
-                    return (
-                        <DatePicker
-                            label={'Date of birth'}
-                            labelMobile={'Date of birthday'}
-                            error={errors.dateOfBirth?.message}
-                            errorLink={<Link href={Path.PrivacyPolicy}>PrivacyPolicy</Link>}
-                            {...field}
-                        />
-                    )
-                }}
-            />
+      <div className={s.selects}>
+        <Controller
+          name='country'
+          control={control}
+          render={({field}) => {
+            return (
+              <Select
+                options={Object.keys(countriesCities)}
+                placeholder='Country'
+                title={'Select your country'}
+                propsValue={dataProfile?.country}
+                {...field}
+              />
+            )
+          }}
+        />
 
-            <div className={s.selects}>
-                <Controller
-                    name='country'
-                    control={control}
-                    render={({field}) => {
-                        return (
-                            <Select
-                                options={Object.keys(countriesCities)}
-                                placeholder='Country'
-                                title={'Select your country'}
-                                propsValue={dataProfile?.country}
-                                {...field}
-                            />
-                        )
-                    }}
-                />
+        <Controller
+          name='city'
+          control={control}
+          render={({field}) => {
+            const cities = selectedCountry ? getCitiesForCountry(selectedCountry) : []
 
-                <Controller
-                    name='city'
-                    control={control}
-                    render={({field}) => {
-                        const cities = selectedCountry ? getCitiesForCountry(selectedCountry) : []
+            return (
+              <Select
+                options={cities}
+                placeholder={selectedCountry ? 'Select city' : 'First select country'}
+                title={'Select your city'}
+                propsValue={dataProfile?.city}
+                disabled={!selectedCountry}
+                {...field}
+              />
+            )
+          }}
+        />
+      </div>
 
-                        return (
-                            <Select
-                                options={cities}
-                                placeholder={selectedCountry ? 'Select city' : 'First select country'}
-                                title={'Select your city'}
-                                propsValue={dataProfile?.city}
-                                disabled={!selectedCountry}
-                                {...field}
-                            />
-                        )
-                    }}
-                />
-            </div>
+      <TextArea label={'About me'} name={'aboutMe'} rows={4} register={register} error={errors.aboutMe?.message}/>
 
-            <TextArea label={'About me'} name={'aboutMe'} rows={4} register={register} error={errors.aboutMe?.message}/>
-
-            <div className={s.line}></div>
-            <div className={s.btnSaved}>
-                <Button type={'submit'} disabled={isLoading}>
-                    {isLoading ? 'Saving...' : 'Save Changes'}
-                </Button>
-            </div>
-        </form>
-    );
+      <div className={s.line}></div>
+      <div className={s.btnSaved}>
+        <Button type={'submit'} disabled={isLoading}>
+          {isLoading ? 'Saving...' : 'Save Changes'}
+        </Button>
+      </div>
+    </form>
+  );
 }
 
