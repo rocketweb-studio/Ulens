7f309abec58b597f99e1271bac525b7660b27d94
YuryR <myaccount135@gmail.com>
2025-10-21 23:56:27 +0300

    refactor: add Sidebar on ui kit, position Sidebar.tsx, position Header.tsx

--- src/shared/ui/AppScroll/AppScroll.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import React from 'react'
-import { Scrollbars } from 'react-custom-scrollbars'
-
-export const AppScroll = ({ children }: { children: React.ReactNode }) => {
-  return <Scrollbars height={'100%'}>{children}</Scrollbars>
-}
