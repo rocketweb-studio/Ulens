a22d1e96a9565a8988b0f9643a421e9929e19778
Sergei-SLS <lyamin.sls@gmail.com>
2025-11-12 21:04:46 +0500

    feat: add endpoints likePost and unLikePost

--- src/entities/post/api/postsApi.ts
+++ src/entities/post/api/postsApi.ts
@@ -70,12 +70,29 @@ export const postsApi = baseApi.injectEndpoints({
       },
       invalidatesTags: ['GetPostsByUsedId'],
     }),
-    getFollowingsPosts: build.query<GetPostsByUserIdResponse, { pageSize: string, endCursorPostId: string }>({
+    getFollowingsPosts: build.query<GetPostsByUserIdResponse, { pageSize: string; endCursorPostId: string }>({
       query: (body) => ({
         method: 'get',
         url: `posts/followings`,
-        params: body
+        params: body,
       }),
+    }),
+    getLikePost: build.mutation<void, { postId: string }>({
+      query: ({ postId }) => ({
+        method: 'POST',
+        url: `/posts/${postId}/like`,
+      }),
+      invalidatesTags: ['GetPostsByUsedId'],
+      // (result, error, {postId}) => [{ type: 'Post', id: postId}],
+    }),
+
+    deleteLikePost: build.mutation<void, { postId: string }>({
+      query: ({ postId }) => ({
+        method: 'DELETE',
+        url: `/posts/${postId}/unlike`,
+      }),
+      invalidatesTags: ['GetPostsByUsedId'],
+      // (result, error, {postId}) => [{ type: 'Post', id: postId}],
     }),
   }),
 })
@@ -87,5 +104,7 @@ export const {
   useDeletePostMutation,
   useUpdatePostMutation,
   useUploadPostImagesMutation,
-  useGetFollowingsPostsQuery
+  useGetFollowingsPostsQuery,
+  useGetLikePostMutation,
+  useDeleteLikePostMutation,
 } = postsApi
