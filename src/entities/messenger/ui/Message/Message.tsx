09c5c030ba795f65e3c95cf59488f50590d090bc
Ilja Afanasev <chylmanprosto@gmail.com>
2025-11-29 19:57:51 +0300

    feat: create base structure Messenger entity

---
+++ src/entities/messenger/ui/Message/Message.tsx
@@ -0,0 +1,5 @@
+import s from './message.module.scss'
+
+export const Message = () => {
+  return <div className={s.message}></div>
+}
---
+++ src/entities/messenger/ui/Messenger.tsx
@@ -0,0 +1,10 @@
+import { FlexContainer } from '@rocketweb-studio/ulens-ui-kit'
+import s from './messenger.module.scss'
+
+export const Messenger = () => {
+  return (
+    <FlexContainer>
+      <div className={s.messenger}></div>
+    </FlexContainer>
+  )
+}
