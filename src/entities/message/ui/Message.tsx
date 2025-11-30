40be99f04b008e067c07611a1f68cbe6041b5c8e
Ilja Afanasev <chylmanprosto@gmail.com>
2025-11-30 21:53:20 +0300

    feat: add styles and structure Message

--- src/entities/message/ui/Message.tsx
+++ src/entities/message/ui/Message.tsx
@@ -1,5 +1,24 @@
 import s from './message.module.scss'
+import { UserAvatar } from '@/src/entities/userProfile'
 
-export const Message = () => {
-  return <div className={s.message}></div>
+type Props = {
+  type: 'mine' | 'friend'
+  message: string
+  date: string
+  avatar: string
+  friendName: string
+}
+
+export const Message = ({ type, message, date, avatar, friendName }: Props) => {
+  return (
+    <div className={`${s.message} ${friendName === 'mine' ? s.mine : ''}`}>
+      {type === 'friend' && (
+        <UserAvatar mode={'size'} width={36} height={36} avatarOwner={avatar} userName={friendName} />
+      )}
+      <div className={s.messageContent}>
+        <span className={s.messageContentText}>{message}</span>
+        <span className={s.messageContentDate}>{date}</span>
+      </div>
+    </div>
+  )
 }
