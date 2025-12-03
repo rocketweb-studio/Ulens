294aab151bc65f581cb3244e315f37f7dc5df90f
Ilja Afanasev <chylmanprosto@gmail.com>
2025-12-03 16:45:26 +0300

    fix: protect messenger route

--- src/views/messengerPage/ui/messengerPage.tsx
+++ src/views/messengerPage/ui/messengerPage.tsx
@@ -3,8 +3,15 @@
 import { MessengerWidget } from '@/src/widgets/messenger'
 import { FlexContainer } from '@rocketweb-studio/ulens-ui-kit'
 import s from './messengerPage.module.scss'
+import { useGetMeQuery } from '@/src/entities/auth/api/authApi'
 
 export const MessengerPage = () => {
+  const { isSuccess } = useGetMeQuery()
+
+  if (!isSuccess) {
+    return null
+  }
+
   return (
     <FlexContainer className={s.wrapper} wrap direction={'column'}>
       <h2 className={s.title}>Messenger</h2>
